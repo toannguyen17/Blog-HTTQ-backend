f7df9e37717960fb3184c9a3e6c333fdec52ab90
vhl <levuhai207@hotmail.com.vn>
2020-10-09 08:30:27 +0700

    hoan thanh tag api

--- src/main/java/com/httq/services/tag/TagService.java
+++ src/main/java/com/httq/services/tag/TagService.java
@@ -4,8 +4,11 @@ import com.httq.model.Tag;
 import com.httq.services.IGeneralService;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
 @Service
 public interface TagService extends IGeneralService<Tag> {
    Integer countByTag(String tag);
    void deleteByTag(String tag);
+   Optional<Tag> findByTag(String tag);
 }
